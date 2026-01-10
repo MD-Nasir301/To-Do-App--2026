dbcece537be4f9a45834a254fc74c29d1bca0ee5
nasir21b@gmail.com <nasir21b@gmail.com>
2026-01-10 21:55:00 +0600

    Add: Date and time in header section

--- js/main.js
+++ js/main.js
@@ -1,7 +1,28 @@
-window.addEventListener('DOMContentLoaded', () => {
-//********** */
+window.addEventListener("DOMContentLoaded", () => {
+  //********** */
 
+  const timeEl = getDomByID("time-value");
+  const dateEl = getDomByID("date-value");
 
+  // Set Date
 
-//********* */
+  setInterval(() => {
+    timeEl.textContent = new Date().toLocaleTimeString([], {
+      hour: "2-digit",
+      minute: "2-digit",
+      //second: "2-digit",
+    });
+
+    dateEl.textContent = new Date().toLocaleDateString("en-UK", {
+      year: "numeric",
+      day: "numeric",
+      month: "numeric",
+    });
+  }, 1000);
+
+  // All Functions
+  function getDomByID(id) {
+    return document.getElementById(id);
+  }
+  //********* */
 });
