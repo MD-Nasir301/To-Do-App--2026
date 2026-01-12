bf05a7699a61b74f5d0cf9e7b5464fbc8f6b3c01
nasir21b@gmail.com <nasir21b@gmail.com>
2026-01-13 01:03:10 +0600

    Done And Remove Fuction added

--- js/main.js
+++ js/main.js
@@ -4,6 +4,131 @@ window.addEventListener("DOMContentLoaded", () => {
   const timeEl = getDomByID("time-value");
   const dateEl = getDomByID("date-value");
   const darkBtn = getDomByID("flexSwitchCheckChecked");
+  const taskDisplayItem = getDomByID("task-text-area");
+  const taskAddBtn = getDomByID("add");
+  const taskInput = getDomByID("task-input");
+  const taskDisplayArea = getDomByID("task-display-area");
+  const doneBtn = getDomByID("done");
+
+  // Primary source of truth for all tasks
+
+  //===================================================
+  let tasksStore = [
+    {
+      date: "01-01-2026",
+      time: "10:00 AM",
+      title: "Task 1",
+    },
+    {
+      date: "02-01-2026",
+      time: "12:30 PM",
+      title: "Task 2",
+    },
+    {
+      date: "03-01-2026",
+      time: "12:30 PM",
+      title: "Task 3",
+    },
+    {
+      date: "01-01-2026",
+      time: "12:30 PM",
+      title: "Task 4",
+    },
+  ];
+//====================================================================
+  // let grouped = [];
+
+  // tasksStore.forEach((obj) => {
+  //   if ([obj.date]) {
+  //     grouped.push({ [obj.date]: obj.time });
+  //   }
+  // });
+  // console.log(grouped);
+
+  //==============================================================^^^^^^
+
+
+  tasksStore.map(obj => {
+    
+  })
+
+  // Handles task creation and updates the primary tasks store
+  taskAddBtn.addEventListener("click", () => {
+    let taskText = taskInput.value;
+    let taskTime = new Date().toLocaleTimeString([], {
+      hour: "2-digit",
+      minute: "2-digit",
+      hour12: true,
+    });
+
+    let taskDate = new Date().toLocaleDateString("en-UK", {
+      year: "numeric",
+      month: "numeric",
+      day: "numeric",
+    });
+
+    let task = {
+      date: taskDate,
+      time: taskTime,
+      title: taskText,
+    };
+    tasksStore.push(task);
+
+    taskDisplayArea.innerHTML = `
+        <ul id="task-list-group"
+              class="task-display-area mt-3 list-unstyled"
+            >
+              <time class="Task-group-date ms-2 tex px-2" datetime=""
+                >25-25-2026</time
+              >
+              <li class="single-task-wrapper mb-2">
+                <div class="container">
+                  <div class="row">
+                    <div class="col-9 col-sm-10 p-0">
+                      <div
+                        class="task-text-area  d-flex ps-2 justify-content-between"
+                        id="task-text-area"
+                      >
+                        <span class="task-item-display"> ${task.title} </span>
+                        <span class="task-time  ">${task.time}</span>
+                      </div>
+                    </div>
+                    <div
+                      class="col-3 col-sm-2 task-btn-area d-flex justify-content-center"
+                    >
+                      <button title="done" id="done" class="done btn">
+                        ☑️
+                      </button>
+                      <button title="Delete" class="delete btn">❌</button>
+                    </div>
+                  </div>
+                </div>
+              </li>
+            </ul>
+    `;
+
+    taskDone();
+    taskRemove();
+  });
+  // Task completed Function
+  function taskDone() {
+    const taskTextArea = document.querySelector(".task-text-area ");
+    taskDisplayArea.addEventListener("click", (e) => {
+      if (e.target.classList.contains("done")) {
+        taskTextArea.classList.toggle("task-completed");
+      }
+    });
+  }
+
+  // Task remove Function
+  function taskRemove() {
+    const singleTaskWrapper = document.querySelector(".single-task-wrapper");
+    taskDisplayArea.addEventListener("click", (e) => {
+      if (e.target.classList.contains("delete")) {
+        singleTaskWrapper.remove();
+      }
+    });
+  }
 
   // Dark Mode Toggle
   darkBtn.addEventListener("click", () => {
